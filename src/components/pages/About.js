67adb76f1407be8399127bb545ff3c78641ef3ee
vijaymanikanta444 <vijayakp444@gmail.com>
2020-11-29 00:14:18 +0530

    about modified

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,6 +5,15 @@ const About = () => {
     <Fragment>
       <h1>About This App</h1>
       <p>App to search Github users</p>
+      <h1>Developer : vijay</h1>
+      <span className="inline">
+        <h4>image</h4>
+        <img
+          src="https://avatars2.githubusercontent.com/u/66003036?v=4"
+          alt="vijay"
+          className="vijay"
+        />
+      </span>
       <p>Version: 1.0.0</p>
     </Fragment>
   );
